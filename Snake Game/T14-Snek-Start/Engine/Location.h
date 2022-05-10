e9554d882a74b7eea52bf470807cf28b0bf6e209
robinflew <104038599+robinflew@users.noreply.github.com>
2022-05-10 16:50:53 -0400

    Add files via upload

---
+++ Snake Game/T14-Snek-Start/Engine/Location.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Location
+{
+public:
+	void Add(const Location& val)
+	{
+		x += val.x;
+		y += val.y;
+
+	}
+	bool operator==(const Location& rhs) const
+	{
+		return x == rhs.x && y == rhs.y;
+	}
+	int x;
+	int y;
+};
